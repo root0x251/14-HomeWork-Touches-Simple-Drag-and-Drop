c39bacf77a2c4e199180cd025213eff0e9bcb9cf
Slava <rtootik@gmail.com>
2017-04-14 10:28:30 +0300

    Initial Commit

---
+++ 14-HomeWork-Touches-Simple-Drag-and-DropTests/_4_HomeWork_Touches_Simple_Drag_and_DropTests.m
@@ -0,0 +1,39 @@
+//
+//  _4_HomeWork_Touches_Simple_Drag_and_DropTests.m
+//  14-HomeWork-Touches-Simple-Drag-and-DropTests
+//
+//  Created by Slava on 14.04.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _4_HomeWork_Touches_Simple_Drag_and_DropTests : XCTestCase
+
+@end
+
+@implementation _4_HomeWork_Touches_Simple_Drag_and_DropTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
